1ebcb83785328827dd9e6284e244b843484d9c7e
JairoPC <jairo9slopez@gmail.com>
2019-05-22 00:11:41 +0200

    Añadimos escribirPlaza y escribirVehiculo

--- src/main/java/daw/Jairo/modelo/Singleton.java
+++ src/main/java/daw/Jairo/modelo/Singleton.java
@@ -50,86 +50,87 @@ public class Singleton {
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }}
+     // Añadir las dependencias de maven en el proyecto
 
-    public static ArrayList<PlazaVO> leerFicheroPlaza(String url) {
-        //creop una lista de App
-        ArrayList<PlazaVO> listaApp = new ArrayList<>();
-
-        // Archivo xml que hay que leer
-        File archivoXML = new File(url);
-
-        // Instancia un documento JDOM usando el SAX (Simple API for XML)
-        // El siguiente objeto permite la lectura del fichero XML y obtener su contenido
-        SAXBuilder constructorXML = new SAXBuilder();
-
-        try {
-            // Cargamos el árbol de nodos del fichero XML
-            Document arbolNodos = (Document) constructorXML.build(archivoXML);
-
-            // Obtenemos el nodo raíz del documento
-            Element nodoRaiz = arbolNodos.getRootElement();
-
-            // Se crea una lista de hijos tipo Element del nodo raíz
-            List<Element> lista = nodoRaiz.getChildren();
-
-            // Por cada hijo que hay en la lista
-            for (Element node : lista) {
-
-                // Guardo en un objeto Mueble la información
-                PlazaVO tmp = new PlazaVO();
-
-                //meto los set del objeto
-                
-                //lo añado a la lista
-                listaApp.add(tmp);
-
-            }
-
-        } catch (IOException | JDOMException io) {
-            System.out.println(io.getMessage());
-        }
-
-        //devuelvo la lista
-        return listaApp;
-    }
-    public static ArrayList<VehiculoVO> leerFicheroVehiculo(String url) {
-        //creop una lista de App
-        ArrayList<VehiculoVO> listaApp = new ArrayList<>();
-
-        // Archivo xml que hay que leer
-        File archivoXML = new File(url);
-
-        // Instancia un documento JDOM usando el SAX (Simple API for XML)
-        // El siguiente objeto permite la lectura del fichero XML y obtener su contenido
-        SAXBuilder constructorXML = new SAXBuilder();
-
-        try {
-            // Cargamos el árbol de nodos del fichero XML
-            Document arbolNodos = (Document) constructorXML.build(archivoXML);
-
-            // Obtenemos el nodo raíz del documento
-            Element nodoRaiz = arbolNodos.getRootElement();
-
-            // Se crea una lista de hijos tipo Element del nodo raíz
-            List<Element> lista = nodoRaiz.getChildren();
-
-            // Por cada hijo que hay en la lista
-            for (Element node : lista) {
-
-                // Guardo en un objeto Mueble la información
-                VehiculoVO tmp = new VehiculoVO();
-                //meto los set del objeto
-                
-                //lo añado a la lista
-                listaApp.add(tmp);
-
-            }
-
-        } catch (IOException | JDOMException io) {
-            System.out.println(io.getMessage());
-        }
-
-        //devuelvo la lista
-        return listaApp;
-    }
+//    public static ArrayList<PlazaVO> leerFicheroPlaza(String url) {
+//        //creop una lista de App
+//        ArrayList<PlazaVO> listaApp = new ArrayList<>();
+//
+//        // Archivo xml que hay que leer
+//        File archivoXML = new File(url);
+//
+//        // Instancia un documento JDOM usando el SAX (Simple API for XML)
+//        // El siguiente objeto permite la lectura del fichero XML y obtener su contenido
+//        SAXBuilder constructorXML = new SAXBuilder();
+//
+//        try {
+//            // Cargamos el árbol de nodos del fichero XML
+//            Document arbolNodos = (Document) constructorXML.build(archivoXML);
+//
+//            // Obtenemos el nodo raíz del documento
+//            Element nodoRaiz = arbolNodos.getRootElement();
+//
+//            // Se crea una lista de hijos tipo Element del nodo raíz
+//            List<Element> lista = nodoRaiz.getChildren();
+//
+//            // Por cada hijo que hay en la lista
+//            for (Element node : lista) {
+//
+//                // Guardo en un objeto Mueble la información
+//                PlazaVO tmp = new PlazaVO();
+//
+//                //meto los set del objeto
+//                
+//                //lo añado a la lista
+//                listaApp.add(tmp);
+//
+//            }
+//
+//        } catch (IOException | JDOMException io) {
+//            System.out.println(io.getMessage());
+//        }
+//
+//        //devuelvo la lista
+//        return listaApp;
+//    }
+//    public static ArrayList<VehiculoVO> leerFicheroVehiculo(String url) {
+//        //creop una lista de App
+//        ArrayList<VehiculoVO> listaApp = new ArrayList<>();
+//
+//        // Archivo xml que hay que leer
+//        File archivoXML = new File(url);
+//
+//        // Instancia un documento JDOM usando el SAX (Simple API for XML)
+//        // El siguiente objeto permite la lectura del fichero XML y obtener su contenido
+//        SAXBuilder constructorXML = new SAXBuilder();
+//
+//        try {
+//            // Cargamos el árbol de nodos del fichero XML
+//            Document arbolNodos = (Document) constructorXML.build(archivoXML);
+//
+//            // Obtenemos el nodo raíz del documento
+//            Element nodoRaiz = arbolNodos.getRootElement();
+//
+//            // Se crea una lista de hijos tipo Element del nodo raíz
+//            List<Element> lista = nodoRaiz.getChildren();
+//
+//            // Por cada hijo que hay en la lista
+//            for (Element node : lista) {
+//
+//                // Guardo en un objeto Mueble la información
+//                VehiculoVO tmp = new VehiculoVO();
+//                //meto los set del objeto
+//                
+//                //lo añado a la lista
+//                listaApp.add(tmp);
+//
+//            }
+//
+//        } catch (IOException | JDOMException io) {
+//            System.out.println(io.getMessage());
+//        }
+//
+//        //devuelvo la lista
+//        return listaApp;
+//    }
 }
