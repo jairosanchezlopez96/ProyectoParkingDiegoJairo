d7df2d48693e806f9a23d5706a0a24bbdd7f9b0c
Whiz <diegofernandez29400@gmail.com>
2019-05-21 10:28:10 +0200

    Borrado del atributo descripcion

--- src/main/java/daw/Jairo/modelo/VehiculoVO.java
+++ src/main/java/daw/Jairo/modelo/VehiculoVO.java
@@ -9,13 +9,11 @@ public class VehiculoVO {
     private int codvehiculo;
     private String matricula;
     private int tipo_Vehiculo;
-    private String descripcion;
 
-    public VehiculoVO(int codvehiculo, String matricula, int tipo_Vehiculo, String descripcion) {
+    public VehiculoVO(int codvehiculo, String matricula, int tipo_Vehiculo) {
         this.codvehiculo = codvehiculo;
         this.matricula = matricula;
         this.tipo_Vehiculo = tipo_Vehiculo;
-        this.descripcion = descripcion;
     }
 
     public VehiculoVO() {
@@ -45,18 +43,10 @@ public class VehiculoVO {
     public void setTipo_Vehiculo(int tipo_Vehiculo) {
         this.tipo_Vehiculo = tipo_Vehiculo;
     }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
+    
     @Override
     public String toString() {
-        return "VehiculoVO{" + "codvehiculo=" + codvehiculo + ", matricula=" + matricula + ", tipo_vehiculo=" + tipo_Vehiculo + ", descripcion=" + descripcion + '}';
+        return "VehiculoVO{" + "codvehiculo=" + codvehiculo + ", matricula=" + matricula + ", tipo_vehiculo=" + tipo_Vehiculo+ '}';
     }
 
     
