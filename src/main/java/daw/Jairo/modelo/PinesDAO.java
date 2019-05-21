c273e28b2a2816f6a0af0e172d1dff68919011ac
Whiz <diegofernandez29400@gmail.com>
2019-05-21 12:41:48 +0200

    Añadimos getAllPines(Jairo)

--- src/main/java/daw/Jairo/modelo/PinesDAO.java
+++ src/main/java/daw/Jairo/modelo/PinesDAO.java
@@ -5,7 +5,11 @@
  */
 package daw.Jairo.modelo;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,10 +17,39 @@ import java.util.List;
  * @author whizrxt
  */
 public class PinesDAO implements IPines {
+private Connection con = null;
 
+    public PinesDAO() {
+        con = Conexion.getInstance();
+    }
     @Override
     public List<PinesVO> getAllPines() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          List<PinesVO> lista = new ArrayList<>();
+
+        // Preparamos la consulta de datos mediante un objeto Statement
+        // ya que no necesitamos parametrizar la sentencia SQL
+        try (Statement st = con.createStatement()) {
+            // Ejecutamos la sentencia y obtenemos las filas en el objeto ResultSet
+            ResultSet res = st.executeQuery("select * from Reserva");
+            // Ahora construimos la lista, recorriendo el ResultSet y mapeando los datos
+            while (res.next()) {
+                PinesVO p = new PinesVO();
+                // Recogemos los datos de la persona, guardamos en un objeto
+              p.setCod_Vehiculo(res.getInt("cod_Vehiculo"));
+                p.setPen_Desechable(res.getString("pen_Desechable"));
+                 p.setNum_Plaza(res.getInt("num_Vehiculo"));
+               
+                p.setFec_Fin_Pin(res.getTimestamp("fec_Fin_Pin").toLocalDateTime());
+                p.setFec_In_Pin(res.getTimestamp("fec_In_Pin").toLocalDateTime());
+                 p.setCoste(res.getDouble("coste"));
+                        
+             
+                //Añadimos el objeto a la lista
+                lista.add(p);
+            }
+        }
+
+        return lista;
     }
 
     @Override
