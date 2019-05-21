c7c19c5efdd7deaef1f88cb71bed2cfa4ad093bb
Whiz <diegofernandez29400@gmail.com>
2019-05-21 12:36:55 +0200

    Terminamos la interfaz IPines y añandimos PinesDAO

---
+++ src/main/java/daw/Jairo/modelo/PinesDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.Jairo.modelo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author whizrxt
+ */
+public class PinesDAO implements IPines {
+
+    @Override
+    public List<PinesVO> getAllPines() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int insertPin(PinesVO pin) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int insertPines(List<PinesVO> pines) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int deletePines(PinesVO pines) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updatePin(int pk, PinesVO pin) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int deletePines() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
