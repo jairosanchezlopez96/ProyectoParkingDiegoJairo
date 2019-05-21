6ec4ae5af6297ee8ed4424a71634482ca731a0ad
JairoSL <jairo9slopez@gmail.com>
2019-05-21 10:49:40 +0200

    Añadimos ClienteVO y sus atributos

---
+++ src/main/java/daw/Jairo/modelo/ClienteVO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.Jairo.modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author jairo
+ */
+public class ClienteVO {
+    private int cod_Cliente;
+    private LocalDate fec_in_abono;
+     
+    private LocalDate fec_fin_abono;
+    private String nombre;
+    private String tarjeta;
+    private int tipo_Abono;
+    private String email;
+    
+}
