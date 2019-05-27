c4b1ec61b970c821679cc82838e269e39ce4da67
Whiz <diegofernandez29400@gmail.com>
2019-05-27 12:02:17 +0200

    Creacion y borrado de objetos

--- src/main/java/daw/Jairo/aplicacion/ProgramaPines.java
+++ src/main/java/daw/Jairo/aplicacion/ProgramaPines.java
@@ -22,19 +22,19 @@ public class ProgramaPines {
     public static void main(String[] args) {
         PinesDAO daoPines = new PinesDAO();
         List<PinesVO> listaPines = new ArrayList<>();
-        listaPines.add(new PinesVO(10, 1, "SFSF", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now()));
+        listaPines.add(new PinesVO(10, 2, "SFSF", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now()));
         listaPines.add(new PinesVO(11, 2, "GDHB", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now()));
         PinesVO p1 = new PinesVO(14, 3, "EJCB", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now());
         PinesVO p10 = new PinesVO(15, 5, "IBGG", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now());
         PinesVO p11 = new PinesVO(20, 6, "FULW", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now());
-        PinesVO p12 = new PinesVO(21, 8, "WOND", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now());
-        listaPines.add(p1);
-        listaPines.add(p12);
+        PinesVO p13 = new PinesVO(14, 3, "FDBF", 123.1, LocalDate.now(), LocalTime.now(), LocalDate.now(), LocalTime.now());
         listaPines.add(p11);
         listaPines.add(p10);
 
         try {
 
+            System.out.println("---Borramos una persona-----");
+            daoPines.deletePines(p13);
             System.out.println("Nº reservas insertadas " + daoPines.insertPines(listaPines));
             System.out.println("-----------------------------------------");
             System.out.println("Comprobamos en una nueva lista que se recogen los datos desde la tabla.");
@@ -42,14 +42,12 @@ public class ProgramaPines {
             System.out.println("-------- Lista con datos recogidos desde la B.D -------------");
             nuevaLista.forEach(System.out::println);
             System.out.println("Insertar una nueva Reserva");
-            daoPines.insertPin(p12);
+            daoPines.insertPin(p13);
             nuevaLista.forEach(System.out::println);
             nuevaLista = daoPines.getAllPines();
 
             System.out.println("-------- Lista con datos recogidos desde la B.D -------------");
             nuevaLista.forEach(System.out::println);
-            System.out.println("---Borramos una persona-----");
-            daoPines.deletePines(p1);
             nuevaLista = daoPines.getAllPines();
             System.out.println("-------- Lista despues de Borrar-------------");
             nuevaLista.forEach(System.out::println);
@@ -57,7 +55,7 @@ public class ProgramaPines {
             // FALTA EL UPDATE PLAZA
             System.out.println("Actualizar una reserva");
 
-            daoPines.updatePin(20, 20, LocalDate.now(), LocalTime.now(), p12);
+            daoPines.updatePin(20, 20, LocalDate.now(), LocalTime.now(), p13);
 
             nuevaLista = daoPines.getAllPines();
             System.out.println("-------- Lista despues actualizar -------------");
