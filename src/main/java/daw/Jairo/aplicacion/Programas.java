21c152d8f3428dc9eae7a019257b83067aa0a3ee
Whiz <diegofernandez29400@gmail.com>
2019-05-27 12:09:25 +0200

    Creacion de un programa que contiene 5 listas

---
+++ src/main/java/daw/Jairo/aplicacion/Programas.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.Jairo.aplicacion;
+
+import daw.Jairo.modelo.ClienteVO;
+import daw.Jairo.modelo.PinesVO;
+import daw.Jairo.modelo.PlazaVO;
+import daw.Jairo.modelo.ReservaVO;
+import daw.Jairo.modelo.VehiculoVO;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author whizrxt
+ */
+public class Programas {
+
+    public static void main(String[] args) {
+        //LISTA DE CLIENTES
+        List<ClienteVO> listaCliente = new ArrayList<>();
+
+        //LISTA DE PINES
+        List<PinesVO> listaPines = new ArrayList<>();
+
+        //LISTA DE PLAZAS
+        List<PlazaVO> listaPlaza = new ArrayList<>();
+
+        //LISTA DE RESERVAS
+        List<ReservaVO> listaReserva = new ArrayList<>();
+
+        //LISTA DE VEHICULOS
+        List<VehiculoVO> listaVehiculo = new ArrayList<>();
+    }
+}
