8ecc8ae05f1b3099000d7e6278d1f4127691b7a2
Whiz <diegofernandez29400@gmail.com>
2019-05-28 13:06:13 +0200

    Creado de constructor, getters y setters

--- src/main/java/daw/Jairo/aplicacion/Programas.java
+++ src/main/java/daw/Jairo/aplicacion/Programas.java
@@ -11,28 +11,78 @@ import daw.Jairo.modelo.PlazaVO;
 import daw.Jairo.modelo.ReservaVO;
 import daw.Jairo.modelo.VehiculoVO;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author whizrxt
  */
 public class Programas {
+    //LISTA DE CLIENTES
 
-    public static void main(String[] args) {
-        //LISTA DE CLIENTES
-        List<ClienteVO> listaCliente = new ArrayList<>();
+    private ArrayList<ClienteVO> listaCliente;
 
-        //LISTA DE PINES
-        List<PinesVO> listaPines = new ArrayList<>();
+    //LISTA DE PINES
+    private ArrayList<PinesVO> listaPines;
+    
+    //LISTA DE PLAZAS
+    private ArrayList<PlazaVO> listaPlaza;
 
-        //LISTA DE PLAZAS
-        List<PlazaVO> listaPlaza = new ArrayList<>();
+    //LISTA DE RESERVAS
+    private ArrayList<ReservaVO> listaReserva;
 
-        //LISTA DE RESERVAS
-        List<ReservaVO> listaReserva = new ArrayList<>();
+    //LISTA DE VEHICULOS
+    private ArrayList<VehiculoVO> listaVehiculo;
 
-        //LISTA DE VEHICULOS
-        List<VehiculoVO> listaVehiculo = new ArrayList<>();
+    public Programas(ArrayList<ClienteVO> listaCliente, ArrayList<PinesVO> listaPines, ArrayList<PlazaVO> listaPlaza, ArrayList<ReservaVO> listaReserva, ArrayList<VehiculoVO> listaVehiculo) {
+        this.listaCliente = listaCliente;
+        this.listaPines = listaPines;
+        this.listaPlaza = listaPlaza;
+        this.listaReserva = listaReserva;
+        this.listaVehiculo = listaVehiculo;
+    }
+
+    public ArrayList<ClienteVO> getListaCliente() {
+        return listaCliente;
+    }
+
+    public void setListaCliente(ArrayList<ClienteVO> listaCliente) {
+        this.listaCliente = listaCliente;
+    }
+
+    public ArrayList<PinesVO> getListaPines() {
+        return listaPines;
+    }
+
+    public void setListaPines(ArrayList<PinesVO> listaPines) {
+        this.listaPines = listaPines;
+    }
+
+    public ArrayList<PlazaVO> getListaPlaza() {
+        return listaPlaza;
+    }
+
+    public void setListaPlaza(ArrayList<PlazaVO> listaPlaza) {
+        this.listaPlaza = listaPlaza;
+    }
+
+    public ArrayList<ReservaVO> getListaReserva() {
+        return listaReserva;
+    }
+
+    public void setListaReserva(ArrayList<ReservaVO> listaReserva) {
+        this.listaReserva = listaReserva;
+    }
+
+    public ArrayList<VehiculoVO> getListaVehiculo() {
+        return listaVehiculo;
+    }
+
+    public void setListaVehiculo(ArrayList<VehiculoVO> listaVehiculo) {
+        this.listaVehiculo = listaVehiculo;
+    }
+
+    @Override
+    public String toString() {
+        return "Programas{" + "listaCliente=" + listaCliente + ", listaPines=" + listaPines + ", listaPlaza=" + listaPlaza + ", listaReserva=" + listaReserva + ", listaVehiculo=" + listaVehiculo + '}';
     }
 }
